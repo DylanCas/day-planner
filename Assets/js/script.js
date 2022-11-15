327380c2c7a430b3eece7211e7eaeb6db17e0c95
DylanCas <djcasabona12@gmail.com>
2022-11-15 00:59:48 -0700

    figured out jquery to add event listener and save text area input to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,19 +9,14 @@ $('#currentDay').text(currentTime);
 var timeBlockArray = ['9','10','11','12','13','14','15','16','17'];
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-  // use for loop to iterate through buttons and add event listeners to all(look into jquery .on)
-  var saveBtn = document.getElementsByClassName("saveBtn");
-  var eventDescription = document.querySelector("#description");
-  saveBtn.addEventListener('click', function(){
-    localStorage.setItem()
-  });
-
   
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+  $(".saveBtn").on("click", function() {
+    // local storage key = saveBtn parent, the div and id 'hour-#'
+    // local storage value = saveBtn prev element, the textarea and its text value
+    localStorage.setItem($(this).parent().attr("id"), $(this).prev().val())
+  });
+  console.log(localStorage) // unsure what "undefined" comes from in results? Does not appear to affect anything
 
-  // takes current time and time array to grab relevant HTML timeblock
-  // TODO: fix 24hr bug, past time 12 noon, all timeblocks are green(future)
   for (var i = 0; i < timeBlockArray.length; i++) {
     let time = timeBlockArray[i] * 1;
     var timeBlock = document.querySelector('#hour-' + time)
@@ -34,13 +29,11 @@ $(function () {
     } else if (time > currentTime24Hr) {
       timeBlock.classList.add("future");/*future = green*/
     }
-    // TODO: use setInterval to update css color styling
       
   }
 
   // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-  // place getItem towards top, needs similar loop 
-  var textArea = document.getElementsByClassName("description");
-  localStorage.getItem();
-  textArea.textContent = 
+  // var textArea = document.getElementsByClassName("description");
+  // localStorage.getItem();
+  // textArea.textContent = 
 });
