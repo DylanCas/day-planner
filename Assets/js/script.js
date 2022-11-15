6886e9a764e39167e2da5f22eb87c44a3474a512
DylanCas <djcasabona12@gmail.com>
2022-11-14 23:43:07 -0700

    Corrected bug in function to apply css colors based on time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,7 +10,7 @@ var timeBlockArray = ['9','10','11','12','13','14','15','16','17'];
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-  // Unsure how to make use of the ID attribute so far
+  // use for loop to iterate through buttons and add event listeners to all(look into jquery .on)
   var saveBtn = document.getElementsByClassName("saveBtn");
   var eventDescription = document.querySelector("#description");
   saveBtn.addEventListener('click', function(){
@@ -25,7 +25,7 @@ $(function () {
   for (var i = 0; i < timeBlockArray.length; i++) {
     let time = timeBlockArray[i] * 1;
     var timeBlock = document.querySelector('#hour-' + time)
-    var currentTime24Hr = (dayjs(currentTime).format('HH')) * 1
+    var currentTime24Hr = (dayjs().format('H')) * 1
       console.log(currentTime24Hr, time)
     if (time === currentTime24Hr) {
       timeBlock.classList.add("present");/*present = red*/
@@ -34,11 +34,12 @@ $(function () {
     } else if (time > currentTime24Hr) {
       timeBlock.classList.add("future");/*future = green*/
     }
+    // TODO: use setInterval to update css color styling
       
   }
 
   // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-  // Unsure how to make use of the ID attribute so far
+  // place getItem towards top, needs similar loop 
   var textArea = document.getElementsByClassName("description");
   localStorage.getItem();
   textArea.textContent = 
